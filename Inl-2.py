b78c1e8a3505c6f8e7b122d94d6ee40943c5f2df
Emma <dat14eas@student.lu.se>
2017-05-03 22:02:07 +0200

    Add files via upload

---
+++ Inl-2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  3 20:31:41 2017
+
+@author: emma
+"""
+import math
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import scipy.interpolate as cs
+from scipy.optimize import fsolve
+
+
+def task1():
+    J = 0; # the heat flux in cal/(cm^2·s)
+    k = 0.01; # cal/(cm·s·◦C)
+    T = ([70, 70, 55, 22, 13, 10, 10]) # temperature in ◦C
+    z = ([0, 0.5, 1, 1.5, 2, 2.5, 3]) # the depth in m
+    
+    
+    
+task1()
